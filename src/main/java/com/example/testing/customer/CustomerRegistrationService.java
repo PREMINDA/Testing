1641697467635ef1ee25bc29a10625aae71bfa27
PREMINDA <premindabandara0777@gmail.com>
2023-03-19 03:25:30 +0530

    User register

--- src/main/java/com/example/testing/customer/CustomerRegistrationService.java
+++ src/main/java/com/example/testing/customer/CustomerRegistrationService.java
@@ -1,7 +1,11 @@
 package com.example.testing.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class CustomerRegistrationService {
 
     private final CustomerRepository customerRepository;
@@ -9,5 +13,19 @@ public class CustomerRegistrationService {
     @Autowired
     public CustomerRegistrationService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+    }
+
+    public void registerNewCustomer(CustomerRegistrationRequest request){
+        String phoneNumber = request.getCustomer().getPhoneNumber();
+        Optional<Customer> customerOptional = customerRepository
+                .selectCustomerByPhoneNumber(phoneNumber);
+        if(customerOptional.isPresent()){
+            Customer customer = customerOptional.get();
+            if(customer.getName().equals(request.getCustomer().getName())){
+                return;
+            }
+            throw new IllegalStateException(String.format("phone number [%s] is taken",phoneNumber));
+        }
+        customerRepository.save(request.getCustomer());
     }
 }
