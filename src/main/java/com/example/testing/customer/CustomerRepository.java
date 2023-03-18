1e40314f94e0b7c0c0568f278778872b96fae27b
PREMINDA <premindabandara0777@gmail.com>
2023-03-19 03:00:27 +0530

    DB config

--- src/main/java/com/example/testing/customer/CustomerRepository.java
+++ src/main/java/com/example/testing/customer/CustomerRepository.java
@@ -1,8 +1,21 @@
 package com.example.testing.customer;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface CustomerRepository extends CrudRepository<Customer, UUID> {
+
+    @Query(
+            value = "select id ,name ,phone_number"+
+            "from customer where phone_number = :phone_number",
+            nativeQuery = true
+    )
+    Optional<Customer> selectCustomerByPhoneNumber(
+            @Param("phone_number") String phoneNUmber);
 }
---
+++ src/test/java/com/example/testing/customer/CustomerRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.testing.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest(
+        properties = {
+                "spring.jpa.properties.javax.persistence.validation.mode=none"
+        }
+)
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository underTest;
+
+    @Test
+    void itShouldSelectCustomerByPhoneNumber() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    void itShouldSaveCustomer() {
+        //Given
+        //When
+        //Then 
+    }
+}
