b9d3546127b8c0786ec9dddb641f1b63dd71a310
PREMINDA <premindabandara0777@gmail.com>
2023-03-23 03:28:48 +0530

    test chargeCard implimentation

---
+++ src/test/java/com/example/testing/payment/PaymentServiceTest.java
@@ -0,0 +1,85 @@
+package com.example.testing.payment;
+
+import com.example.testing.customer.Customer;
+import com.example.testing.customer.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+class PaymentServiceTest {
+
+    @Mock
+    private CustomerRepository customerRepository;
+    @Mock
+    private PaymentRepository paymentRepository;
+    @Mock
+    private  CardPaymentCharger cardPaymentCharger;
+    private static final List<Currency> ACCEPTED_CURRENCIES = List.of(Currency.USD, Currency.GBP);
+
+    private PaymentService underTest;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+        underTest = new PaymentService(customerRepository,paymentRepository,cardPaymentCharger);
+    }
+
+    @Test
+    void itShouldCardChargeSuccessfully() {
+        //Given
+        UUID customerId = UUID.randomUUID();
+
+        // ... Customer exists
+        given(customerRepository.findById(customerId)).willReturn(Optional.of(mock(Customer.class)));
+
+        // ... Payment request
+        PaymentRequest paymentRequest = new PaymentRequest(
+                new Payment(
+                        null,
+                        null,
+                        new BigDecimal("100.00"),
+                        Currency.USD,
+                        "card123xx",
+                        "Donation"
+                )
+        );
+
+        // ... Card is charged successfully
+        given(cardPaymentCharger.chargeCard(
+                paymentRequest.getPayment().getSource(),
+                paymentRequest.getPayment().getAmount(),
+                paymentRequest.getPayment().getCurrency(),
+                paymentRequest.getPayment().getDescription()
+        )).willReturn(new CardPaymentCharge(true));
+
+        // When
+        underTest.chargeCard(customerId, paymentRequest);
+
+        // Then
+        ArgumentCaptor<Payment> paymentArgumentCaptor =
+                ArgumentCaptor.forClass(Payment.class);
+
+        then(paymentRepository).should().save(paymentArgumentCaptor.capture());
+
+        Payment paymentArgumentCaptorValue = paymentArgumentCaptor.getValue();
+        assertThat(paymentArgumentCaptorValue)
+                .usingRecursiveComparison()
+                .ignoringFields("customerId")
+                .isEqualTo(paymentRequest.getPayment());
+
+        assertThat(paymentArgumentCaptorValue.getCustomerId()).isEqualTo(customerId);
+    }
+}
